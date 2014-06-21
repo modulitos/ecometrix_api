9fb1076044816ebe0161108505093e912eb5e834
Luke Swart <lukeswart@gmail.com>
2014-06-21 09:44:10 -0700

    Implemented basic login validation.

--- public/login/webapp_login.js
+++ public/login/webapp_login.js
@@ -1,44 +1,64 @@
-function verifyLogin() {
-        // If using an ID other than #email or #error then replace it here
-        // email = $("#email");       
-        var errornotice = $("#error");
-        // The text to show up within a field when it is incorrect
-        var emptyerror = "Please fill out this field.";
-        // var emailerror = "Please enter a valid e-mail.";
+function getUserInfo() {
+    // If using an ID other than #email or #error then replace it here
+    // email = $("#email");       
+    var errornotice = $("#error");
+    // The text to show up within a field when it is incorrect
+    var emptyerror = "Please fill out this field.";
+    // var emailerror = "Please enter a valid e-mail.";
 
-        // Add the fields
-        var userTokens = {
-            data: []
-        };
-        userTokens.data.push({
-            username: $('#username').val(),
-            password: $('#password').val()
-        });
+    // Add the fields
+    var userTokens = {
+        data: []
+    };
+    userTokens.data.push({
+        username: $('#username').val(),
+        password: $('#password').val()
+    });
 
-        console.log("verifying input: ");
-        console.log(userTokens);
-        console.log(JSON.stringify(userTokens));
+    console.log("verifying input: ");
+    console.log(userTokens);
+    console.log(JSON.stringify(userTokens));
 
-        var username = userTokens.data[0].username;
-        var password = userTokens.data[0].password;
-        var md5_password = $.md5(userTokens.data.password);
-        console.log("username: " + username);
-        console.log("password: " + password);
-        console.log("md5_password: " + md5_password);
+    var username = userTokens.data[0].username;
+    var password = userTokens.data[0].password;
+    var md5_password = $.md5(password);
+    console.log("username: " + username);
+    console.log("password: " + password);
+    console.log("md5_password: " + md5_password);
 
-        var userinfo = {
-            username: username,
-            password: md5_password
-        };
+    var userinfo = {
+        username: username,
+        password: md5_password
+    };
+    return userinfo;
+} // End verify login
+
+$(document).ready(function() {
+
+    /* attach a submit handler to the form */
+    $('#userinfo').submit(function(event) {
+
+        /* stop form from submitting normally */
+        event.preventDefault();
+
+        var userinfo = getUserInfo();
         console.log("sending userinfo to server: ");
         console.log(userinfo);
+
         $.ajax({
             type: 'GET',
             data: jQuery.param(userinfo),
             contentType: 'application/json; charset=utf-8',
             url: '/login/verify'
         }).done(function(response) {
-            alert(response.msg);
+            if (response.msg == '') {
+                // alert(response.msg);
+                var message = "Welcome to Ecometrix, " + userinfo.username + "!";
+                alert(message);
+            }
+            else {
+                alert("error: " + response.msg);
+            }
         });
 
         // Clears any fields in the form when the user clicks on them
@@ -46,17 +66,8 @@ function verifyLogin() {
             // $('#username').val('');
             // $('#password').val('');
             $(this).closest('form').find("input[type=text], textarea").val("");
-        }); 
+        });
 
-} // End verify login
-
-$(document).ready(function() {
-
-    /* attach a submit handler to the form */
-    $('#userinfo').submit(function(event) {
-        /* stop form from submitting normally */
-        event.preventDefault();
-        verifyLogin();
         return true;
     });
 });
