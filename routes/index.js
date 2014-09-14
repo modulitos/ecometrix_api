b35741b2d37415557044f0a2857e765cabb336d9
Luke Swart <lukeswart@gmail.com>
2014-09-14 05:53:51 -0700

    Reconfigured directory structure. Set app as the new landing page, removed express test as landing page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Luke\'s Express Test' });
+    // res.render('index', { title: 'Luke\'s Express Test' });
+    // res.render('index', {title: 'Luke\'s Express Test'}, function(err, html) {
+    //     console.log(html);
+    //     res.send('done');
+    // });
+    // res.sendFile(path.join(__dirname, '../public', 'app.html'));
+    // res.sendfile('../public/app.html');
+    // res.sendFile('app.html', { root: path.join(__dirname, '../public') });
+    
+    res.sendfile('public/app.html');
 });
 
 module.exports = router;
